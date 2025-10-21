2799f82aedc67da74d6b10e56c0166c1e2c0d626
Ushnish Chowdhury <ushnishchowdhury62@gmail.com>
2025-10-21 21:29:03 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,7 @@ export default function RootLayout({
         <FirebaseClientProvider>
           <div className="flex min-h-screen w-full flex-col">
             <AppHeader />
-            <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-              {children}
-            </main>
+            {children}
           </div>
           <Toaster />
         </FirebaseClientProvider>
