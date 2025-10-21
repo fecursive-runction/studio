8195ebe99ecfd3350a14d04f20b7986ecdee9b2b
Ushnish Chowdhury <ushnishchowdhury62@gmail.com>
2025-10-21 21:45:01 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
   "projectId": "kiln-ai-28281",
   "appId": "1:453629474251:web:e41d8b635c9a7852c086d3",
+  "storageBucket": "kiln-ai-28281.appspot.com",
   "apiKey": "AIzaSyBlbEaWJjXJZAcoiV9RzM8-l2k3pEprXQY",
   "authDomain": "kiln-ai-28281.firebaseapp.com",
-  "storageBucket": "kiln-ai-28281.appspot.com",
   "messagingSenderId": "453629474251"
 };
